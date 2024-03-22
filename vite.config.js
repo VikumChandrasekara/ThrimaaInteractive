40b1d3eea130b5aa4e040c5597e71d99a7ba44e5
Vikum Chandrasekara <chandrasekara.vikum@gmail.com>
2024-03-22 20:44:17 +0530

    fix: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/ThrimaaInteractive/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
