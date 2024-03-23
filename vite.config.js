0e5884a86a9d772c2c2360532a9d33d597dad239
Vikum Chandrasekara <chandrasekara.vikum@gmail.com>
2024-03-24 02:08:40 +0530

    testing7

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/ThrimaaInteractive/",
+  base:"./ThrimaaInteractive/",
 
-  resolve:{
-    alias:{
-      '@components': '/src/components'
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/': {
+  //       target: 'http://127.0.0.1:5173//ThrimaaInteractive/'
+  //     },
+
+  //     '/portfolio': {
+  //       target: 'http://127.0.0.1:5173//ThrimaaInteractive/portfolio'
+  //     }
+  //   }
+  // },
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
