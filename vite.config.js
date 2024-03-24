67dea95a6c806ba7b8d98de9394b9b459ed8fe5c
Vikum Chandrasekara <chandrasekara.vikum@gmail.com>
2024-03-24 08:07:14 +0530

    testing9

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/ThrimaaInteractive/",
-
+  // base:"/ThrimaaInteractive/",
+  
   plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
   }
 })
+
+export const configForSubdirectory1 = defineConfig({
+  base: '/ThrimaaInteractive/',
+});
+
+export const configForSubdirectory2 = defineConfig({
+  base: '/ThrimaaInteractive/portfolio/',
+});
